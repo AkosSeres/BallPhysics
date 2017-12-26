840ea358b43fbfb732726fb4c89cfcf44559766c
AkosSeres <seresakos2000@gmail.com>
2017-12-27 00:31:39 +0100

    Implementing angular mechanics between balls and balls

--- src/ball.js
+++ src/ball.js
@@ -1,12 +1,15 @@
 class Ball {
-  constructor(pos, vel, r, k) {
+  constructor(pos, vel, r, k, ang) {
     this.pos = pos.copy();
     this.lastPos = this.pos.copy();
     this.r = r;
-    this.fc = 0.3;//coefficient of friction
+    this.fc = 0.4;//coefficient of friction
+    this.am = 1;//angular mass (am*m*r*r)
 
     this.rotation = 0;
-    this.ang = 0;
+
+    if (ang) this.ang = ang;
+    else this.ang = 0;
 
     if (k) this.k = k;
     else this.k = 0.8;
@@ -38,6 +41,7 @@ class Ball {
       var s2 = p5.Vector.sub(pos2, lPos2);
       var dist = p5.Vector.dist(pos1, pos2);
       var lastDist = p5.Vector.dist(lPos1, lPos2);
+      var fc = (ball1.fc + ball2.fc) / 2;
 
       while (lastDist < rSum) {
         var d1 = p5.Vector.sub(pos1, pos2);
@@ -117,11 +121,41 @@ class Ball {
       u2n.div(m1 + m2);
       u2n.sub(p5.Vector.mult(v2n, kk));
 
+      let dv1n = p5.Vector.dist(u1n, v1n);
+      let dv2n = p5.Vector.dist(u2n, v2n);
+
+      let p1 = new Vec2(v1.x, v1.y);
+      let p2 = new Vec2(v2.x, v2.y);
+      let rot = new Vec2(np1.x - np2.x, np1.y - np2.y).heading;
+
+      p1.rotate(-rot + Math.PI / 2);
+      p2.rotate(-rot + Math.PI / 2);
+      let vk = (m1 * p1.x + m2 * p2.x) / (m1 + m2);
+      //let vk = (m1 * (p1.x + ball1.ang * r1) + m2 * (p2.x - ball2.ang * r2)) / (m1 + m2);
+      //vk += (m1 * ball1.ang * r1 - m2 * ball2.ang * r2) / (m1 + m2);
+      vk += (ball1.am * m1 * r1 * r1 * ball1.ang - ball2.am * m2 * r2 * r2 * ball2.ang) / (ball1.am * m1 * r1 + ball2.am * m2 * r2);
+
+      let dv1p = -dv1n * fc * Math.sign(p1.x - ball1.ang * r1 - vk);
+      if (Math.abs(dv1p) > Math.abs(p1.x - ball1.ang * r1 - vk)) {dv1p = -p1.x + ball1.ang * r1 + vk;}
+      let dv2p = -dv2n * fc * Math.sign(p2.x + ball2.ang * r2 - vk);
+      if (Math.abs(dv2p) > Math.abs(p2.x + ball2.ang * r2 - vk)) {dv2p = -p2.x - ball2.ang * r2 + vk;}
+      let dv1 = new Vec2(dv1p + dv1p / (ball1.am + 1), 0);
+      let dv2 = new Vec2(dv2p - dv2p / (ball2.am + 1), 0);
+      dv1.rotate(rot - Math.PI / 2);
+      dv2.rotate(rot - Math.PI / 2);
+
       v1n = u1n;
       v2n = u2n;
 
       ball1.vel = p5.Vector.add(v1n, v1p);
       ball2.vel = p5.Vector.add(v2n, v2p);
+
+      ball1.ang -= dv1p / ((ball1.am + 1) * r1);
+      ball2.ang += dv2p / ((ball2.am + 1) * r2);
+      ball1.vel.x += dv1.x;
+      ball1.vel.y += dv1.y;
+      ball2.vel.x += dv2.x;
+      ball2.vel.y += dv2.y;
 
       ball1.lastPos = cp1;
       ball2.lastPos = cp2;
