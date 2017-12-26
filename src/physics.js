bba3f26e1b17adf53f80f323a4ffd9ac3047f3e0
AkosSeres <seresakos2000@gmail.com>
2017-12-26 16:03:58 +0100

    Collision algorythm wizh walls made simpler

--- src/physics.js
+++ src/physics.js
@@ -18,39 +18,36 @@ class Physics {
       this.balls[i].lastPos = this.balls[i].pos.copy();
       this.balls[i].pos.add(this.balls[i].vel.copy().mult(t));
 
+      //angular vel
+      this.balls[i].rotation += this.balls[0].ang * t;
+      this.balls[i].rotation %= (Math.PI * 2);
+
       //apply gravity
-      this.balls[i].vel.add(this.gravity.copy().mult(t));
+      if (this.gravity) this.balls[i].vel.add(this.gravity.copy().mult(t));
 
       //bounce from the edges
       if (this.bounds != 0) {
         if (this.balls[i].pos.x - this.balls[i].r < this.bounds[0]) {
-          this.balls[i].vel.x *= -this.balls[i].k;
-          var xS = this.balls[i].pos.x - this.balls[i].lastPos.x;
-          this.balls[i].pos.x = 2 * this.bounds[0] - this.balls[i].pos.x + 2 * this.balls[i].r;
-          this.balls[i].lastPos.x = this.balls[i].pos.x + xS;
+          let ball = this.balls[i];
+          ball.vel.x *= -ball.k;
+          ball.pos.x = this.bounds[1] + ball.r;
         } else if (this.balls[i].pos.x + this.balls[i].r > (this.bounds[0] + this.bounds[2])) {
-          this.balls[i].vel.x *= -this.balls[i].k;
-          var xS = this.balls[i].pos.x - this.balls[i].lastPos.x;
-          this.balls[i].pos.x = 2 * (this.bounds[0] + this.bounds[2]) - this.balls[i].pos.x - 2 * this.balls[i].r;
-          this.balls[i].lastPos.x = this.balls[i].pos.x + xS;
+          let ball = this.balls[i];
+          ball.vel.x *= -ball.k;
+          ball.pos.x = (this.bounds[0] + this.bounds[2]) - ball.r;
         }
         if (this.balls[i].pos.y + this.balls[i].r > (this.bounds[1] + this.bounds[3])) {
-          this.balls[i].vel.y *= -this.balls[i].k;
-          var yS = this.balls[i].pos.y - this.balls[i].lastPos.y;
-          this.balls[i].pos.y = 2 * (this.bounds[1] + this.bounds[3]) - this.balls[i].pos.y - 2 * this.balls[i].r;
-          this.balls[i].lastPos.y = this.balls[i].pos.y + yS;
+          let ball = this.balls[i];
+          ball.vel.y *= -ball.k;
+          ball.pos.y = (this.bounds[1] + this.bounds[3]) - ball.r;
         } else if (this.balls[i].pos.y - this.balls[i].r < this.bounds[1]) {
-          this.balls[i].vel.y *= -this.balls[i].k;
-          var yS = this.balls[i].pos.y - this.balls[i].lastPos.y;
-          this.balls[i].pos.y = 2 * this.bounds[1] - this.balls[i].pos.y + 2 * this.balls[i].r;
-          this.balls[i].lastPos.y = this.balls[i].pos.y + yS;
+          let ball = this.balls[i];
+          ball.vel.y *= -ball.k;
+          ball.pos.y = this.bounds[1] + ball.r;
         }
       }
 
       //collision
-      for (var j = i + 1; j < this.balls.length; j++) {
-        Ball.collide(this.balls[i], this.balls[j]);
-      }
       for (var j = i + 1; j < this.balls.length; j++) {
         Ball.collide(this.balls[i], this.balls[j]);
       }
