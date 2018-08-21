55427d6d64029708276c29d813204c15cf6b6f70
AkosSeres <seresakos2000@gmail.com>
2018-08-21 14:49:38 +0200

    Switch jshint to eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,19 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         concat: {
-	        dist: {
-			    files: {
-				    'dist/<%= pkg.name %>.js': 'src/**/*.js'
-			    }
-		    }
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.js': 'src/**/*.js'
+                }
+            }
         },
         babel: {
-	        dist: {
-			    files: {
-				    'dist/<%= pkg.name %>.js': 'dist/<%= pkg.name %>.js'
-			    }
-		    }
-	    },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.js': 'dist/<%= pkg.name %>.js'
+                }
+            }
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
@@ -29,24 +29,26 @@ module.exports = function(grunt) {
                 }
             }
         },
-        jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-            options: grunt.file.readJSON('package.json').jshintConfig
+        eslint: {
+            target: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                useEslintrc: true
+            }
         },
         watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint', 'concat', 'babel']
+            files: ['<%= eslint.target %>'],
+            tasks: ['eslint', 'concat', 'babel']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-babel');
 
-    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('test', ['eslint']);
 
-    grunt.registerTask('default', ['jshint', 'concat', 'babel', 'uglify']);
+    grunt.registerTask('default', ['eslint', 'concat', 'babel', 'uglify']);
 
 };
