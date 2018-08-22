60bbd036f2b0419612982948c5b3486ddfa209f1
AkosSeres <seresakos2000@gmail.com>
2018-08-22 11:02:52 +0200

    Fixing linter errors in stick.js

--- src/stick.js
+++ src/stick.js
@@ -1,19 +1,32 @@
 const Spring = require('./spring');
 const Vec2 = require('./vec2');
 
+/**
+ * Stick class for the physics engine
+ * Sticks are not strechable objects that do not collide
+ * with other objects but they can hold other objects on their ends
+ */
 class Stick extends Spring {
+    /**
+     * Creates a stick
+     * @param {nuber} length The length of the stick
+     */
     constructor(length) {
         super(length);
         this.springConstant = 0;
     }
 
+    /**
+     * Updates the stick trough an elapsed time
+     * @param {number} t Elapsed time
+     */
     update(t) {
-        let p1, p2;
+        let p1;
+        let p2;
         if (this.pinned && this.objects[0]) {
             p2 = this.pinned;
             p1 = this.objects[0];
             let dist = new Vec2(p2.x - p1.pos.x, p2.y - p1.pos.y);
-            let dl = dist.length - this.length;
             dist.setMag(1);
             dist.mult(-this.length);
             p1.pos.x = p2.x + dist.x;
@@ -52,13 +65,18 @@ class Stick extends Spring {
             v1.x = v2.x = (p1.m * v1.x + p2.m * v2.x) / ((p1.m) + (p2.m));
 
             if (this.rotationLocked) {
-                let s = new Vec2(p1.pos.x * p1.m + p2.pos.x * p2.m, p1.pos.y * p1.m + p2.pos.y * p2.m);
+                let s = new Vec2(p1.pos.x * p1.m + p2.pos.x * p2.m,
+                    p1.pos.y * p1.m + p2.pos.y * p2.m);
                 s.div(p1.m + p2.m);
                 let r1 = Vec2.sub(p1.pos, s);
                 let r2 = Vec2.sub(p2.pos, s);
-                let am = r1.length * r1.length * p1.m + p1.am + r2.length * r2.length * p2.m + p2.am;
-                let sv = (v1.y - v2.y) * r2.length / (r1.length + r2.length) + v2.y;
-                let ang = (p1.am * p1.ang + p2.am * p2.ang - r1.length * p1.m * (v1.y - sv) + r2.length * p2.m * (v2.y - sv)) / (am);
+                let am = r1.length * r1.length * p1.m + p1.am +
+                    r2.length * r2.length * p2.m + p2.am;
+                let sv = (v1.y - v2.y) * r2.length /
+                    (r1.length + r2.length) + v2.y;
+                let ang = (p1.am * p1.ang + p2.am * p2.ang -
+                    r1.length * p1.m * (v1.y - sv) +
+                    r2.length * p2.m * (v2.y - sv)) / (am);
 
                 v1.y = -ang * r1.length + sv;
                 v2.y = +ang * r2.length + sv;
