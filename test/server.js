776c10734595ef635fc9fe3a97a2d7e4109f6624
AkosSeres <seresakos2000@gmail.com>
2019-07-10 20:28:54 +0200

    Test code removal

--- test/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('./'));
-app.use('/dist', express.static('../dist'));
-
-app.listen(3000, () => console.log('listening on port 3000!'));
