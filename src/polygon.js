388487079b434fb7f28cd7006862b50d5e889b79
Akos Seres <seresakos2000@gmail.com>
2020-09-01 23:18:38 +0200

    Add more functionality to the Polygon class

--- src/polygon.js
+++ src/polygon.js
@@ -11,7 +11,11 @@ class Polygon {
      * @param {Vec2[]} points_ Array of the points for the polygon in order
      */
     constructor(points_) {
-        if (points_.length < 3) throw new Error('Not enough points in polygon (minimum required: 3)');
+        if (points_.length < 3) {
+            throw new Error(
+                'Not enough points in polygon (minimum required: 3)'
+            );
+        }
         this.points = points_;
         this.makeAntiClockwise();
     }
@@ -19,12 +23,34 @@ class Polygon {
     /**
      * Returns the vector of the side with the given number
      * The vector has the orientation of the order of the points
-     * @param {number} num The vector of the side
+     * @param {number} num The number of the side
      * @return {Vec2} The side vector
      */
     getSideVector(num) {
         if (num < 0) num += Math.abs(Math.floor(num)) * this.points.length;
         return Vec2.sub(this.points[(num + 1) % this.points.length],
+            this.points[num % this.points.length]);
+    }
+
+    /**
+     * Returns the side with the given number as a LineSegment
+     * @param {number} num The number of the side
+     * @return {LineSegment} The side line segment
+     */
+    getSideSegment(num) {
+        if (num < 0) num += Math.abs(Math.floor(num)) * this.points.length;
+        return new LineSegment(this.points[(num + 1) % this.points.length],
+            this.points[num % this.points.length]);
+    }
+
+    /**
+     * Returns the side with the given number as a Line
+     * @param {number} num The number of the side
+     * @return {Line} The side line
+     */
+    getSideLine(num) {
+        if (num < 0) num += Math.abs(Math.floor(num)) * this.points.length;
+        return new Line(this.points[(num + 1) % this.points.length],
             this.points[num % this.points.length]);
     }
 
@@ -69,6 +95,193 @@ class Polygon {
      */
     reverseOrder() {
         this.points = this.points.reverse();
+    }
+
+    /**
+     * Determines if a given point is inside the polygon or not
+     * @param {Vec2} point The point to investigate
+     * @return {boolean} If the point is inside or not
+     */
+    isPointInside(point) {
+        if (Vec2.dist(point, this.centerPoint) > this.boundRadius) return false;
+
+        // Find a point that is outside of the shape for sure
+        let outsidePoint = this.centerPoint.copy;
+        outsidePoint.add(Vec2.mult(new Vec2(1.1, 0.6), this.boundRadius));
+
+        // Create a LineSegment between the points
+        let segment = new LineSegment(point, outsidePoint);
+
+        // Count the intersections
+        let intersectionCount = 0;
+        [...Array(this.sides).keys()].
+            map((number) => this.getSideSegment(number)).forEach((side) => {
+                if (LineSegment.intersect(side, segment)) intersectionCount++;
+            });
+
+        if (intersectionCount % 2 == 0) return false;
+        else if (intersectionCount % 2 == 1) return true;
+    }
+
+    /**
+     * Returns the mathematical average of the point of the polygon
+     * @return {Vec2} The center point
+     */
+    get centerPoint() {
+        let center = new Vec2(0, 0);
+        this.points.forEach((p) => {
+            center.add(p);
+        });
+        center.div(this.sides);
+        return center;
+    }
+
+    /**
+     * Returns the radius of the smallest possible
+     * circle containing the whole polygon centered
+     * around the polygon's centerPoint
+     * The circle's center is the centerPoint of the polygon
+     * @return {number} The radius
+     */
+    get boundRadius() {
+        let center = this.centerPoint;
+        return Math.max(...this.points.map((p) => Vec2.dist(p, center)));
+    }
+
+    /**
+     * @return {LineSegment[]} Array containing the sides of the polygon
+     */
+    get allSides() {
+        return [...Array(this.sides).keys()].
+            map((number) => this.getSideSegment(number));
+    }
+
+    /**
+     * Determines the intersecting area of two polygons
+     * Returns undefined if there is none
+     * @param {Polygon} poly1 The first polygon
+     * @param {Polygon} poly2 The second polygon
+     * @return {Polygon} The intersection area
+     */
+    static intersection(poly1, poly2) {
+        if (Vec2.dist(poly1.centerPoint, poly2.centerPoint) >
+            poly1.boundRadius + poly2.boundRadius) return undefined;
+
+        // Determine intersection points
+        let sideIntersections = [];
+        let sides1 = poly1.allSides;
+        let sides2 = poly2.allSides;
+        sides1.forEach((side1, idx1) => {
+            sides2.forEach((side2, idx2) => {
+                let intersectionOfSides = LineSegment.intersect(side1, side2);
+                if (intersectionOfSides) {
+                    intersectionOfSides['isIntersectionPoint'] = true;
+                    sideIntersections.push({
+                        intersectionPoint: intersectionOfSides,
+                        sideNumber1: idx1,
+                        sideNumber2: idx2,
+                    });
+                };
+            });
+        });
+
+        if (sideIntersections.length === 0) {
+            if (poly1.isPointInside(poly2.points[0])) {
+                return new Polygon(poly2.points.map((p) => p.copy));
+            }
+            if (poly2.isPointInside(poly1.points[0])) {
+                return new Polygon(poly1.points.map((p) => p.copy));
+            }
+        }
+
+        // Put the intersection points into the copy of polygons
+        let new1 = new Polygon(poly1.points.map((p) => p.copy));
+        for (let i = new1.points.length - 1; i >= 0; i--) {
+            let intersectionsOnThis =
+                sideIntersections.filter((it) => it.sideNumber1 == i);
+            if (intersectionsOnThis.length > 1) {
+                intersectionsOnThis.sort((a, b) => {
+                    return Vec2.dist(new1.points[i], a.intersectionPoint)
+                        - Vec2.dist(new1.points[i], b.intersectionPoint);
+                });
+            }
+            if (intersectionsOnThis.length > 0) {
+                new1.points.splice(i + 1, 0,
+                    ...intersectionsOnThis.map((it) => it.intersectionPoint));
+            }
+        }
+        let new2 = new Polygon(poly2.points.map((p) => p.copy));
+        for (let i = new2.points.length - 1; i >= 0; i--) {
+            let intersectionsOnThis =
+                sideIntersections.filter((it) => it.sideNumber2 == i);
+            if (intersectionsOnThis.length > 1) {
+                intersectionsOnThis.sort((a, b) => {
+                    return Vec2.dist(new2.points[i], a.intersectionPoint)
+                        - Vec2.dist(new2.points[i], b.intersectionPoint);
+                });
+            }
+            if (intersectionsOnThis.length > 0) {
+                new2.points.splice(i + 1, 0,
+                    ...intersectionsOnThis.map((it) => it.intersectionPoint));
+            }
+        }
+
+        // Find a starting point outside of the other poly
+        let currInv = {
+            polyNum: 1,
+            pointNum: 0,
+        };
+        for (let i = 0; i < new1.points.length; i++) {
+            if (new1.points[i].isIntersectionPoint) {
+                currInv.pointNum = i;
+                break;
+            } else if (new2.isPointInside(new1.points[i])) {
+                currInv.pointNum = i;
+                break;
+            }
+        }
+        let isDone = false;
+        let finalPoints = [];
+        while (!isDone) {
+            let currentPoly = (currInv.polyNum === 1) ? new1 : new2;
+            let otherPoly = (currInv.polyNum === 1) ? new2 : new1;
+            finalPoints.push(
+                currentPoly.points[
+                    currInv.pointNum % currentPoly.points.length].copy
+            );
+            if (finalPoints.length > 2 &&
+                finalPoints[0].x == finalPoints[finalPoints.length - 1].x &&
+                finalPoints[0].y == finalPoints[finalPoints.length - 1].y) {
+                finalPoints.pop();
+                break;
+            }
+            if (finalPoints.length > new1.points.length + new2.points.length) {
+                break;
+            }
+
+            if (currentPoly.points[currInv.pointNum %
+                currentPoly.points.length].isIntersectionPoint) {
+                if (currentPoly.points[(currInv.pointNum + 1) %
+                    currentPoly.points.length].isIntersectionPoint) {
+                    currInv.pointNum++;
+                } else if (otherPoly.isPointInside(
+                    currentPoly.points[(currInv.pointNum + 1) %
+                    currentPoly.points.length]) &&
+                    !currentPoly.points[(currInv.pointNum + 1) %
+                        currentPoly.points.length].isIntersectionPoint) {
+                    currInv.pointNum++;
+                } else {
+                    currInv.pointNum = otherPoly.points.indexOf(
+                        currentPoly.points[currInv.pointNum %
+                        currentPoly.points.length]) + 1;
+                    currInv.polyNum = (currInv.polyNum === 1) ? 2 : 1;
+                }
+            } else {
+                currInv.pointNum++;
+            }
+        }
+
+        return new Polygon(finalPoints);
     }
 }
 
--- tests/polygon.test.js
+++ tests/polygon.test.js
@@ -15,47 +15,81 @@ test('flip triangle points order if it is clockwise', () => {
 
 test('flip quadrilateral points order if it is clockwise', () => {
     let quadrilateral =
-        new Polygon(
-            [new Vec2(2, 1), new Vec2(1, 4), new Vec2(2, 6), new Vec2(4, 5)]);
+        new Polygon([
+            new Vec2(2, 1),
+            new Vec2(1, 4),
+            new Vec2(2, 6),
+            new Vec2(4, 5),
+        ]);
     expect(quadrilateral)
         .toEqual(
-            new Polygon(
-                [new Vec2(4, 5), new Vec2(2, 6), new Vec2(1, 4), new Vec2(2, 1)]));
+            new Polygon([
+                new Vec2(4, 5),
+                new Vec2(2, 6),
+                new Vec2(1, 4),
+                new Vec2(2, 1),
+            ]));
 
     quadrilateral.makeAntiClockwise();
     expect(quadrilateral).toEqual(
-        new Polygon(
-            [new Vec2(4, 5), new Vec2(2, 6), new Vec2(1, 4), new Vec2(2, 1)]));
+        new Polygon([
+            new Vec2(4, 5),
+            new Vec2(2, 6),
+            new Vec2(1, 4),
+            new Vec2(2, 1),
+        ]));
 });
 
 test('flip square points order if it is clockwise', () => {
     let quadrilateral =
-        new Polygon(
-            [new Vec2(-1, -1), new Vec2(-1, 1), new Vec2(1, 1), new Vec2(1, -1)]);
+        new Polygon([
+            new Vec2(-1, -1),
+            new Vec2(-1, 1),
+            new Vec2(1, 1),
+            new Vec2(1, -1),
+        ]);
     expect(quadrilateral)
-        .toEqual(
-            new Polygon(
-                [new Vec2(1, -1), new Vec2(1, 1), new Vec2(-1, 1), new Vec2(-1, -1)]));
+        .toEqual(new Polygon([
+            new Vec2(1, -1),
+            new Vec2(1, 1),
+            new Vec2(-1, 1),
+            new Vec2(-1, -1),
+        ]));
 
     quadrilateral.makeAntiClockwise();
-    expect(quadrilateral).toEqual(
-        new Polygon(
-            [new Vec2(1, -1), new Vec2(1, 1), new Vec2(-1, 1), new Vec2(-1, -1)]));
+    expect(quadrilateral).toEqual(new Polygon([
+        new Vec2(1, -1),
+        new Vec2(1, 1),
+        new Vec2(-1, 1),
+        new Vec2(-1, -1),
+    ]));
 });
 
 test('flip pentagon points order if it is clockwise', () => {
     let quadrilateral =
-        new Polygon(
-            [new Vec2(2, 1), new Vec2(1, 4), new Vec2(-6, 5), new Vec2(2, 6), new Vec2(4, 5)]);
+        new Polygon([
+            new Vec2(2, 1),
+            new Vec2(1, 4),
+            new Vec2(-6, 5),
+            new Vec2(2, 6),
+            new Vec2(4, 5)]);
     expect(quadrilateral)
         .toEqual(
-            new Polygon(
-                [new Vec2(4, 5), new Vec2(2, 6), new Vec2(-6, 5), new Vec2(1, 4), new Vec2(2, 1)]));
+            new Polygon([
+                new Vec2(4, 5),
+                new Vec2(2, 6),
+                new Vec2(-6, 5),
+                new Vec2(1, 4),
+                new Vec2(2, 1)]));
 
     quadrilateral.makeAntiClockwise();
     expect(quadrilateral).toEqual(
-        new Polygon(
-            [new Vec2(4, 5), new Vec2(2, 6), new Vec2(-6, 5), new Vec2(1, 4), new Vec2(2, 1)]));
+        new Polygon([
+            new Vec2(4, 5),
+            new Vec2(2, 6),
+            new Vec2(-6, 5),
+            new Vec2(1, 4),
+            new Vec2(2, 1)]));
 });
 
 test('flip points of clockwise order polygon', () => {
@@ -86,5 +120,90 @@ test('throw error if number of points is not enough', () => {
     expect(createPolygon([])).toThrow();
     expect(createPolygon([new Vec2(1, 1)])).toThrow();
     expect(createPolygon([new Vec2(1, 1), new Vec2(2, 2)])).toThrow();
-    expect(createPolygon([new Vec2(1, 1), new Vec2(2, 2), new Vec2(3, 1)])).not.toThrow();
+    expect(createPolygon([
+        new Vec2(1, 1),
+        new Vec2(2, 2),
+        new Vec2(3, 1),
+    ])).not.toThrow();
+});
+
+test('right intersection for different polygon orientations', () => {
+    let square1 = new Polygon([
+        new Vec2(-1, -1),
+        new Vec2(1, -1),
+        new Vec2(1, 1),
+        new Vec2(-1, 1),
+    ]);
+    let poly2 = new Polygon([
+        new Vec2(-12, -14),
+        new Vec2(16, -12),
+        new Vec2(12, 18),
+        new Vec2(-13, 18),
+    ]);
+    let poly3 = new Polygon([
+        new Vec2(-12, -14),
+        new Vec2(-16, -12),
+        new Vec2(-12, -18),
+        new Vec2(-13, -18),
+    ]);
+    let poly4 = new Polygon([
+        new Vec2(1, 2),
+        new Vec2(4, 2),
+        new Vec2(4, 3),
+        new Vec2(1, 3),
+    ]);
+    let poly5 = new Polygon([
+        new Vec2(2, -1),
+        new Vec2(3, -1),
+        new Vec2(3, 5),
+        new Vec2(2, 5),
+    ]);
+    let poly6 = new Polygon([
+        new Vec2(2, 3),
+        new Vec2(3, 3),
+        new Vec2(3, 2),
+        new Vec2(2, 2),
+    ].reverse());
+    let poly7 = new Polygon([
+        new Vec2(-2, 3),
+        new Vec2(2, 3),
+        new Vec2(2, -3),
+        new Vec2(-2, -3),
+    ]);
+    let poly8 = new Polygon([
+        new Vec2(-1, -1),
+        new Vec2(-1, -5),
+        new Vec2(-5, -5),
+        new Vec2(-5, -1),
+    ]);
+    let poly9 = new Polygon([
+        new Vec2(-1, -3),
+        new Vec2(-2, -3),
+        new Vec2(-2, -1),
+        new Vec2(-1, -1),
+    ]);
+    let poly10 = new Polygon([
+        new Vec2(0, 0),
+        new Vec2(4, 0),
+        new Vec2(4, 4),
+        new Vec2(0, 4),
+    ]);
+    let poly11 = new Polygon([
+        new Vec2(-2, 1),
+        new Vec2(2, 1),
+        new Vec2(2, 3),
+        new Vec2(-2, 3),
+    ]);
+    let poly12 = new Polygon([
+        new Vec2(2, 1),
+        new Vec2(2, 3),
+        new Vec2(0, 3),
+        new Vec2(0, 1),
+    ]);
+
+    expect(Polygon.intersection(square1, poly2)).toEqual(square1);
+    expect(Polygon.intersection(square1, poly3)).toBe(undefined);
+    expect(Polygon.intersection(poly4, poly5)).toEqual(poly6);
+    expect(Polygon.intersection(poly7, poly8)).toEqual(poly9);
+    expect(Polygon.intersection(poly10, poly11)).toEqual(poly12);
 });
