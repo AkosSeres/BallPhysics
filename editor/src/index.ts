8a408e93fff1ee789272989e85603227e64041a7
Akos Seres <seresakos2000@gmail.com>
2021-01-06 20:38:25 +0100

    Fix path

--- editor/src/index.ts
+++ editor/src/index.ts
@@ -4,7 +4,7 @@ window.onload = () => {
   (window as any).editorApp = new Editor();
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceworker.js').then(() => {
+    navigator.serviceWorker.register('serviceworker.js').then(() => {
     }, (err) => {
       // eslint-disable-next-line no-console
       console.log('ServiceWorker registration failed: ', err);
