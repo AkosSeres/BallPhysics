e02c12666ca4e16d8ebeb9ab32b1aaff4b06764b
Akos Seres <seresakos2000@gmail.com>
2021-02-03 06:02:40 +0100

    Improve stick creator and spring creator modes

--- editor/src/modes/springcreator.tsx
+++ editor/src/modes/springcreator.tsx
@@ -1,14 +1,59 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Mode from '../modeInterface';
-import { PinPoint, Spring } from '../../../src/physics';
+import {
+  Body, PinPoint, Shape, Spring, Vec2,
+} from '../../../src/physics';
 import elementCreator from '../elementCreator';
 import '../components/checkbox';
 import '../components/range-slider';
+import EditorInterface from '../editorInterface';
+import palette from '../../../src/util/colorpalette';
 
 let lockRotation = false;
+let centerSnap = true;
+let startPos = new Vec2(0, 0);
+let startRot = 0;
 let springConstant = 10000;
+const randomSpring = new Spring(1, springConstant);
+randomSpring.attachObject(new Body(Shape.Circle(1, new Vec2(0, 0))));
 const element = document.createElement('div');
+
+/**
+ * @param {EditorInterface} editor The editor
+ * @returns {Vec2} The current pos of the original attach point
+ */
+function convertLastPos(editor: EditorInterface) {
+  const { choosed } = editor;
+  const last = new Vec2(editor.lastX, editor.lastY);
+  if (editor.lastX !== 0 && editor.lastY !== 0 && choosed instanceof Body) {
+    const rel = Vec2.sub(last, startPos);
+    rel.rotate(choosed.rotation - startRot);
+    if (centerSnap) {
+      rel.x = 0;
+      rel.y = 0;
+    }
+    rel.add(choosed.pos);
+    return rel;
+  }
+  return last;
+}
+
+/**
+ * Creates a spring that's starting and ending points are the given vectors.
+ *
+ * @param {Vec2} a The start vector
+ * @param {Vec2} b The end vector
+ * @returns {Spring} The resulting spring
+ */
+function springFromAB(a: Vec2, b: Vec2) {
+  randomSpring.length = a.dist(b);
+  randomSpring.springConstant = springConstant;
+  randomSpring.objects[0].pos = a;
+  randomSpring.objects[0].shape.points[0] = a;
+  randomSpring.pinHere(b.x, b.y);
+  return randomSpring;
+}
 
 const SpringCreatorMode: Mode = {
   name: 'Spring creator',
@@ -16,21 +61,57 @@ const SpringCreatorMode: Mode = {
   element,
   drawFunc(editorApp, dt) {
     const ctx = editorApp.cnv.getContext('2d') as CanvasRenderingContext2D;
+    ctx.save();
 
     if (editorApp.lastX !== 0 && editorApp.lastY !== 0) {
-      ctx.strokeStyle = 'black';
-      ctx.beginPath();
-      ctx.moveTo(editorApp.mouseX, editorApp.mouseY);
-      ctx.lineTo(editorApp.lastX, editorApp.lastY);
-      ctx.stroke();
+      ctx.fillStyle = '#00000000';
+      ctx.strokeStyle = '#FFFFFF';
+
+      const start = convertLastPos(editorApp);
+      const end = new Vec2(editorApp.mouseX, editorApp.mouseY);
+      const springToDraw = springFromAB(start, end);
+
+      editorApp.renderer.renderSpring(springToDraw, ctx);
+    }
+
+    // Highlight on hover
+    const lookedAt = editorApp.physics.getObjectAtCoordinates(
+      editorApp.mouseX, editorApp.mouseY,
+    );
+    if (lookedAt instanceof Body) {
+      ctx.globalAlpha = 0.6;
+      ctx.fillStyle = '#00000000';
+      ctx.strokeStyle = '#FFFFFF';
+      ctx.lineWidth = 3;
+      editorApp.renderer.renderBody(lookedAt, ctx);
+    }
+
+    ctx.restore();
+  },
+  startInteractionFunc(editorApp) {
+    if (editorApp.choosed instanceof Body) {
+      startPos = editorApp.choosed.pos.copy;
+      startRot = editorApp.choosed.rotation;
+    } else if (typeof editorApp.choosed !== 'boolean') {
+      startPos = new Vec2(editorApp.choosed.x, editorApp.choosed.y);
+      startRot = 0;
     }
   },
-  startInteractionFunc(editorApp) { },
   endInteractionFunc(editorApp) {
     if (editorApp.lastX !== 0 && editorApp.lastY !== 0) {
       /** @type {Body | boolean | PinPoint} */
-      let newChoosed = editorApp.physics.getObjectAtCoordinates(editorApp.mouseX, editorApp.mouseY);
+      let newChoosed = editorApp.physics.getObjectAtCoordinates(
+        editorApp.mouseX, editorApp.mouseY,
+      );
       let stick;
+      let attachPoint1 = convertLastPos(editorApp);
+      let attachPoint2 = new Vec2(editorApp.mouseX, editorApp.mouseY);
+      if (editorApp.choosed instanceof Body && centerSnap) {
+        attachPoint1 = editorApp.choosed.pos.copy;
+      }
+      if (newChoosed instanceof Body && centerSnap) {
+        attachPoint2 = newChoosed.pos.copy;
+      }
       const Thing = Spring;
       if (typeof newChoosed === 'boolean') {
         /** @type {PinPoint} */
@@ -57,7 +138,7 @@ const SpringCreatorMode: Mode = {
           ),
           springConstant,
         );
-        stick.attachObject(newChoosed);
+        stick.attachObject(newChoosed, attachPoint2);
         stick.pinHere(editorApp.choosed.x, editorApp.choosed.y);
       } else if (newChoosed instanceof Object && editorApp.choosed instanceof Object
          && 'pos' in editorApp.choosed && 'pinPoint' in newChoosed) {
@@ -68,7 +149,7 @@ const SpringCreatorMode: Mode = {
           ),
           springConstant,
         );
-        stick.attachObject(editorApp.choosed);
+        stick.attachObject(editorApp.choosed, attachPoint1);
         stick.pinHere(newChoosed.x, newChoosed.y);
       } else if (editorApp.choosed instanceof Object && newChoosed instanceof Object
         && 'pos' in editorApp.choosed && 'pos' in newChoosed) {
@@ -79,8 +160,8 @@ const SpringCreatorMode: Mode = {
           ),
           springConstant,
         );
-        stick.attachObject(editorApp.choosed);
-        stick.attachObject(newChoosed);
+        stick.attachObject(editorApp.choosed, attachPoint1);
+        stick.attachObject(newChoosed, attachPoint2);
       }
       if (typeof stick === 'undefined') return;
       editorApp.physics.addSpring(stick);
@@ -94,6 +175,9 @@ const SpringCreatorMode: Mode = {
 element.append(
   <check-box checked={lockRotation} onChange={(newBool: boolean) => { lockRotation = newBool; }}>
     Lock rotation
+  </check-box>,
+  <check-box checked={centerSnap} onChange={(newBool: boolean) => { centerSnap = newBool; }}>
+    Snap to center
   </check-box>,
   <range-slider
     min={2000}
--- editor/src/modes/stickcreator.tsx
+++ editor/src/modes/stickcreator.tsx
@@ -1,13 +1,55 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Mode from '../modeInterface';
-import { Stick } from '../../../src/physics';
+import {
+  Body, Shape, Stick, Vec2, PinPoint,
+} from '../../../src/physics';
 import elementCreator from '../elementCreator';
 import '../components/checkbox';
+import EditorInterface from '../editorInterface';
 
 let lockRotation = false;
-
+let centerSnap = true;
+let startPos = new Vec2(0, 0);
+let startRot = 0;
+const randomStick = new Stick(1);
+randomStick.attachObject(new Body(Shape.Circle(1, new Vec2(0, 0))));
 const element = document.createElement('div');
+
+/**
+ * @param {EditorInterface} editor The editor
+ * @returns {Vec2} The current pos of the original attach point
+ */
+function convertLastPos(editor: EditorInterface) {
+  const { choosed } = editor;
+  const last = new Vec2(editor.lastX, editor.lastY);
+  if (editor.lastX !== 0 && editor.lastY !== 0 && choosed instanceof Body) {
+    const rel = Vec2.sub(last, startPos);
+    rel.rotate(choosed.rotation - startRot);
+    if (centerSnap) {
+      rel.x = 0;
+      rel.y = 0;
+    }
+    rel.add(choosed.pos);
+    return rel;
+  }
+  return last;
+}
+
+/**
+ * Creates a stick that's starting and ending points are the given vectors.
+ *
+ * @param {Vec2} a The start vector
+ * @param {Vec2} b The end vector
+ * @returns {Stick} The resulting spring
+ */
+function stickFromAB(a: Vec2, b: Vec2) {
+  randomStick.length = a.dist(b);
+  randomStick.objects[0].pos = a;
+  randomStick.objects[0].shape.points[0] = a;
+  randomStick.pinHere(b.x, b.y);
+  return randomStick;
+}
 
 const StickCreatorMode: Mode = {
   name: 'Stick creator',
@@ -15,21 +57,55 @@ const StickCreatorMode: Mode = {
   element,
   drawFunc(editorApp, dt) {
     const ctx = editorApp.cnv.getContext('2d') as CanvasRenderingContext2D;
+    ctx.save();
 
     if (editorApp.lastX !== 0 && editorApp.lastY !== 0) {
-      ctx.strokeStyle = 'black';
-      ctx.beginPath();
-      ctx.moveTo(editorApp.mouseX, editorApp.mouseY);
-      ctx.lineTo(editorApp.lastX, editorApp.lastY);
-      ctx.stroke();
+      ctx.fillStyle = '#00000000';
+      ctx.strokeStyle = '#FFFFFF';
+
+      const start = convertLastPos(editorApp);
+      const end = new Vec2(editorApp.mouseX, editorApp.mouseY);
+      const springToDraw = stickFromAB(start, end);
+
+      editorApp.renderer.renderStick(springToDraw, ctx);
+    }
+
+    // Highlight on hover
+    const lookedAt = editorApp.physics.getObjectAtCoordinates(
+      editorApp.mouseX, editorApp.mouseY,
+    );
+    if (lookedAt instanceof Body) {
+      ctx.globalAlpha = 0.6;
+      ctx.fillStyle = '#00000000';
+      ctx.strokeStyle = '#FFFFFF';
+      ctx.lineWidth = 3;
+      editorApp.renderer.renderBody(lookedAt, ctx);
+    }
+
+    ctx.restore();
+  },
+  startInteractionFunc(editorApp) {
+    if (editorApp.choosed instanceof Body) {
+      startPos = editorApp.choosed.pos.copy;
+      startRot = editorApp.choosed.rotation;
+    } else if (typeof editorApp.choosed !== 'boolean') {
+      startPos = new Vec2(editorApp.choosed.x, editorApp.choosed.y);
+      startRot = 0;
     }
   },
-  startInteractionFunc(editorApp) { },
   endInteractionFunc(editorApp) {
     if (editorApp.lastX !== 0 && editorApp.lastY !== 0) {
       /** @type {Body | boolean | PinPoint} */
       let newChoosed = editorApp.physics.getObjectAtCoordinates(editorApp.mouseX, editorApp.mouseY);
       let stick;
+      let attachPoint1 = convertLastPos(editorApp);
+      let attachPoint2 = new Vec2(editorApp.mouseX, editorApp.mouseY);
+      if (editorApp.choosed instanceof Body && centerSnap) {
+        attachPoint1 = editorApp.choosed.pos.copy;
+      }
+      if (newChoosed instanceof Body && centerSnap) {
+        attachPoint2 = newChoosed.pos.copy;
+      }
       const Thing = Stick;
       if (typeof newChoosed === 'boolean') {
         newChoosed = {
@@ -52,7 +128,7 @@ const StickCreatorMode: Mode = {
             + ((editorApp.choosed.y - newChoosed.pos.y) ** 2),
           ),
         );
-        stick.attachObject(newChoosed);
+        stick.attachObject(newChoosed, attachPoint2);
         stick.pinHere(editorApp.choosed.x, editorApp.choosed.y);
       } else if ('pinPoint' in newChoosed && 'pos' in editorApp.choosed) {
         stick = new Thing(
@@ -61,7 +137,7 @@ const StickCreatorMode: Mode = {
             + ((editorApp.choosed.pos.y - newChoosed.y) ** 2),
           ),
         );
-        stick.attachObject(editorApp.choosed);
+        stick.attachObject(editorApp.choosed, attachPoint1);
         stick.pinHere(newChoosed.x, newChoosed.y);
       } else if ('pos' in editorApp.choosed && 'pos' in newChoosed) {
         stick = new Thing(
@@ -70,8 +146,8 @@ const StickCreatorMode: Mode = {
             + ((editorApp.choosed.pos.y - newChoosed.pos.y) ** 2),
           ),
         );
-        stick.attachObject(editorApp.choosed);
-        stick.attachObject(newChoosed);
+        stick.attachObject(editorApp.choosed, attachPoint1);
+        stick.attachObject(newChoosed, attachPoint2);
       }
       if (typeof stick === 'undefined') return;
       editorApp.physics.addSpring(stick);
@@ -87,6 +163,9 @@ const StickCreatorMode: Mode = {
 [
   <check-box checked={lockRotation} onChange={(newBool: boolean) => { lockRotation = newBool; }}>
     Lock rotation
+  </check-box>,
+  <check-box checked={centerSnap} onChange={(newBool: boolean) => { centerSnap = newBool; }}>
+    Snap to center
   </check-box>,
 ].forEach(element.appendChild.bind(element));
 
